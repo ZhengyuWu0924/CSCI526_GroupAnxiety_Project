eede8b8728a1492c03abaaaa64ca61b892ac7235
Zhengyu Wu <zywwu@ucdavis.edu>
2023-02-09 17:31:52 -0800

    fix: analysis ver1.1
    add the url link in file

--- Assets/Scripts/Analysis/SendToGoogle.cs
+++ Assets/Scripts/Analysis/SendToGoogle.cs
@@ -6,6 +6,7 @@ using UnityEngine.Networking;
 
 public class SendToGoogle : MonoBehaviour
 {
+    // URL is https://docs.google.com/forms/u/4/d/e/1FAIpQLSdOFLieYVjan5B-iP79DfZ8M6S5c_thnbpF3sSTJM9rvcxOTg/formResponse
     [SerializeField] private string URL;
     private long _sessionID;
     private float _inkUsage;
