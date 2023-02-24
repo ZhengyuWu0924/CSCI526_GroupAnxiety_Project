c687fe484db88436ed6729febfd6aea2eacb2b9c
matudinglin <45813154+matudinglin@users.noreply.github.com>
2023-02-23 20:50:45 -0800

    Update Singleton.cs

--- Assets/Scripts/Utilities/Singleton.cs
+++ Assets/Scripts/Utilities/Singleton.cs
@@ -3,21 +3,43 @@ using System.Collections.Generic;
 using UnityEngine;
 
 /// <summary>
-/// Ensure the object is unique and persistent through scenes
+/// ReplaceableSingleton overrides the current instance.
 /// </summary>
-public abstract class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+public abstract class ReplaceableSingleton<T> : MonoBehaviour where T : MonoBehaviour
 {
     public static T Instance { get; private set; }
     protected virtual void Awake()
     {
-        if (Instance != null) Destroy(gameObject);
         Instance = this as T;
-        DontDestroyOnLoad(gameObject);
     }
 
     protected virtual void OnApplicationQuit()
     {
         Instance = null;
         Destroy(gameObject);
+    }
+}
+
+/// <summary>
+/// Basic Singleton.
+/// </summary>
+public abstract class Singleton<T> : ReplaceableSingleton<T> where T : MonoBehaviour
+{
+    protected override void Awake()
+    {
+        if (Instance != null) Destroy(gameObject);
+        base.Awake();
+    }
+}
+
+/// <summary>
+/// PersistentSingleton will stay across very levels.
+/// </summary>
+public abstract class PersistentSingleton<T> : Singleton<T> where T : MonoBehaviour
+{
+    protected override void Awake()
+    {
+        base.Awake();
+        DontDestroyOnLoad(gameObject);
     }
 }
