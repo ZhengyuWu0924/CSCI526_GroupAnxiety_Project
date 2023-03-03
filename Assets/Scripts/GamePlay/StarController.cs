eaf0284ea43dd51d16ccbd5d1077a493ddf96ac5
ywj128 <wanjingy128@gmail.com>
2023-03-02 21:20:24 -0800

    Add star mechanism

---
+++ Assets/Scripts/GamePlay/StarController.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StarController : MonoBehaviour
+{
+    private GameManager gameManager;
+    private void Start()
+    {
+        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            gameManager.updateCurrentLevelStars(GameManager.currentLevelStars + 1);
+            Destroy(this.gameObject);
+        }
+    }
+}
