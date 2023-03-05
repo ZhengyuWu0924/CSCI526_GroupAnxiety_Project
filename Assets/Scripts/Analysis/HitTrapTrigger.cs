72c6da39e84e30f5a539577c7171ba466fe9aa00
ywj128 <wanjingy128@gmail.com>
2023-03-04 23:55:41 -0800

    Update invincible effect

--- Assets/Scripts/Analysis/HitTrapTrigger.cs
+++ Assets/Scripts/Analysis/HitTrapTrigger.cs
@@ -8,7 +8,7 @@ public class HitTrapTrigger : MonoBehaviour
     // private TrapFormToGoogle tftg;
     private string collisionTrap;
     [SerializeField]
-    private float InvincibleTime = 3;
+    private float invincibleTime = 3;
     private bool isInvincible = false;
     void Start(){
         gm = FindObjectOfType<GameManager>();
@@ -17,7 +17,7 @@ public class HitTrapTrigger : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision){
         if (collision.gameObject.CompareTag("Trap") && !isInvincible){
-            startInvincible();
+            StartInvincible();
             gm.updateInk(GetComponent<PlayerController>().remainInk * 0.333f);
             collisionTrap = collision.gameObject.name;
             switch(collisionTrap){
@@ -35,19 +35,31 @@ public class HitTrapTrigger : MonoBehaviour
             }
         }
     }
-    private void startInvincible()
+    private void StartInvincible()
     {
         isInvincible = true;
-        Color tmp = GetComponent<SpriteRenderer>().color;
-        tmp.a = 0.6f;
-        GetComponent<SpriteRenderer>().color = tmp;
-        Invoke("endInvincible", InvincibleTime);
+        StartCoroutine(InvinsibleEffect());
     }
-    private void endInvincible()
+    IEnumerator InvinsibleEffect()
     {
+        for(int i=0; i<invincibleTime; i++)
+        {
+            StartCoroutine(FadeTo(0.6f, 0.5f));
+            yield return new WaitForSeconds(0.5f);
+            StartCoroutine(FadeTo(1, 0.5f));
+            yield return new WaitForSeconds(0.5f);
+        }
         isInvincible = false;
-        Color tmp = GetComponent<SpriteRenderer>().color;
-        tmp.a = 1;
-        GetComponent<SpriteRenderer>().color = tmp;
+    }
+    IEnumerator FadeTo(float aValue, float aTime)
+    {
+        Color color = GetComponent<SpriteRenderer>().color;
+        float alpha = GetComponent<SpriteRenderer>().color.a;
+        for (float t = 0.0f; t < 1.0f; t += Time.deltaTime / aTime)
+        {
+            color.a = Mathf.Lerp(alpha, aValue, t);
+            GetComponent<SpriteRenderer>().color = color;
+            yield return null;
+        }
     }
 }
