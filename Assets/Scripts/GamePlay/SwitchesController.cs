9bc01dbfe66e88e0b29948986cc5092bbc840c2a
JunhaoDeng <junhaode@usc.edu>
2023-03-05 19:50:02 -0800

    Update SwitchesController.cs

--- Assets/Scripts/GamePlay/SwitchesController.cs
+++ Assets/Scripts/GamePlay/SwitchesController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class SwitchesController : MonoBehaviour
 {
     public List<GameObject> objectsToDisappear;
+    private bool isOn = false; 
     // Start is called before the first frame update
     void Start()
     {
@@ -21,10 +22,24 @@ public class SwitchesController : MonoBehaviour
     {
         if (collision.gameObject.CompareTag("Mutable Object") || collision.gameObject.CompareTag("Player"))
         {
-
-            foreach(GameObject obj in objectsToDisappear)
+            isOn = !isOn;
+            foreach (GameObject obj in objectsToDisappear)
             {
-                Destroy(obj);
+                SpriteRenderer renderer = obj.GetComponent<SpriteRenderer>();
+                BoxCollider2D collider = obj.GetComponent<BoxCollider2D>();
+
+                // if the switch is on, make the game object transparent and disable the collider
+                if (isOn)
+                {
+                    renderer.color = new Color(renderer.color.r, renderer.color.g, renderer.color.b, 0f);
+                    collider.enabled = false;
+                }
+                // if the switch is off, make the game object fully visible and enable the collider
+                else
+                {
+                    renderer.color = new Color(renderer.color.r, renderer.color.g, renderer.color.b, 1f);
+                    collider.enabled = true;
+                }
             }
         }
     }
