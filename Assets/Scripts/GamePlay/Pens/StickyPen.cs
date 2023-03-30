4effcc865dba75d7ab17745b86481089b065c551
matudinglin <45813154+matudinglin@users.noreply.github.com>
2023-03-30 11:06:30 -0700

    Sticky Pen demo and Sticky Pen sample scene

---
+++ Assets/Scripts/GamePlay/Pens/StickyPen.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StickyPen : BasicPen
+{
+    //public int cornerVertics;
+    //public int capVertics;
+    public override void InitializePen()
+    {
+        base.InitializePen();
+        base.penName = "StickyPen";
+    }
+
+    private FixedJoint2D joint;
+    private GameObject otherObject;
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        //if (collision.gameObject.CompareTag("Mutable Object"))
+        //{
+            otherObject = collision.gameObject;
+        //}
+
+        if (otherObject != null)
+        {
+            joint = new FixedJoint2D();
+
+            joint = gameObject.AddComponent<FixedJoint2D>();
+
+            joint.connectedBody = otherObject.GetComponent<Rigidbody2D>();
+
+            joint.breakForce = Mathf.Infinity;
+        }
+    }
+
+}
