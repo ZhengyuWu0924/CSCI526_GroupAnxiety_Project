6f368b341b56d3fce8467c59d8dc53d89b1793f8
ywj128 <wanjingy128@gmail.com>
2023-04-09 01:33:49 -0700

    Add flashing effect on ink bar
    The ink bar flashes when the remain ink is low

--- Assets/Scripts/UI/RemainInkSliderControl.cs
+++ Assets/Scripts/UI/RemainInkSliderControl.cs
@@ -7,9 +7,13 @@ public class RemainInkSliderControl : MonoBehaviour
 {
 
     private Slider slider;
+    private bool ifflash;
+    private GameObject fillColor;
+    [SerializeField]private float flashAlpha = 0.4f;
     void Awake()
     {
         slider = GetComponent<Slider>();
+        fillColor = gameObject.transform.Find("InkFillColor").gameObject;
     }
 
     // Update is called once per frame
@@ -27,4 +31,38 @@ public class RemainInkSliderControl : MonoBehaviour
         slider.maxValue = remainInk;
     }
 
+    private void Update()
+    {
+        if(!ifflash && slider.value < slider.maxValue / 4)
+        {
+            ifflash = true;
+            StartCoroutine(FlashWhenLowInk());
+        }
+        if(slider.value >= slider.maxValue / 4)
+        {
+            ifflash = false;
+        }
+    }
+
+    IEnumerator FlashWhenLowInk()
+    {
+        while(ifflash)
+        {
+            StartCoroutine(FadeTo(flashAlpha, 0.25f));
+            yield return new WaitForSeconds(0.25f);
+            StartCoroutine(FadeTo(1, 0.25f));
+            yield return new WaitForSeconds(0.25f);
+        }
+    }
+    IEnumerator FadeTo(float aValue, float aTime)
+    {
+        Color color = fillColor.GetComponent<Image>().color;
+        float alpha = fillColor.GetComponent<Image>().color.a;
+        for (float t = 0.0f; t < 1.0f; t += Time.deltaTime / aTime)
+        {
+            color.a = Mathf.Lerp(alpha, aValue, t);
+            fillColor.GetComponent<Image>().color = color;
+            yield return null;
+        }
+    }
 }
