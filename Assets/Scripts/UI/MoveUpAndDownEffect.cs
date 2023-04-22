963b5b1490f73b3fc7ab2626cf1a5d24cb09987e
ywj128 <wanjingy128@gmail.com>
2023-04-21 19:46:22 -0700

    update move up and down effect

--- Assets/Scripts/UI/MoveUpAndDownEffect.cs
+++ Assets/Scripts/UI/MoveUpAndDownEffect.cs
@@ -4,22 +4,25 @@ using UnityEngine;
 
 public class MoveUpAndDownEffect : MonoBehaviour
 {
+    [SerializeField] private bool ifMoveOnAwake = true;
+    [SerializeField] private int specificMovingTimes;
     [SerializeField] private float movingHeight = 0.6f;
     [SerializeField] private float movingTime = 0.8f;
     private float originalY;
-    public bool ifMove;
+    
     // Start is called before the first frame update
     void Start()
     {
-        ifMove = true;
         originalY = transform.position.y;
-        StartCoroutine(MoveUpAndDown());
+        if (ifMoveOnAwake)
+        {
+            StartMoving();
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
     }
     IEnumerator MoveY(float yValue, float yTime)
     {
@@ -32,12 +35,35 @@ public class MoveUpAndDownEffect : MonoBehaviour
     }
     IEnumerator MoveUpAndDown()
     {
-        while (ifMove)
+        while (true)
         {
             StartCoroutine(MoveY(originalY + movingHeight, movingTime));
             yield return new WaitForSeconds(movingTime);
             StartCoroutine(MoveY(originalY, movingTime));
             yield return new WaitForSeconds(movingTime);
         }
+    }
+
+    public void StartMoving()
+    {
+        StartCoroutine(MoveUpAndDown());
+    }
+
+    public void StopMoving()
+    {
+        StopAllCoroutines();
+    }
+
+    public void SpecifyMove()
+    {
+        // Object stop moving after several move
+        StartCoroutine(MoveSpecificTimes());
+    }
+
+    IEnumerator MoveSpecificTimes()
+    {
+        StartMoving();
+        yield return new WaitForSeconds(2 * movingTime * specificMovingTimes);
+        StopMoving();
     }
 }
